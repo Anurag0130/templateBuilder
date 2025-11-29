60bd462ceae2bc934e4de3257457f34ed952a8f6
ArvajSaifi <arvajkhan.edunext@gmail.com>
2025-11-29 11:16:32 +0530

    app file work

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        
         <Route path="/" element={<TemplatePreview />} />
         <Route path="/main" element={<TemplateBuilder />} />
       </Routes>
