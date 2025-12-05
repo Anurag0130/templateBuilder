58cf9ecc47ab93bd6239fce24ce881df9058cf2e
Anurag0130 <thakuranurag.edunext@gmail.com>
2025-12-05 10:46:31 +0530

    canvas work

--- src/pages/TemplateBuilder.jsx
+++ src/pages/TemplateBuilder.jsx
@@ -35,7 +35,9 @@ export default function TemplateBuilder() {
     const canRedo = currentPage?.historyIndex < currentPage?.history?.length - 1;
 
 
-
+    console.log('this is element', currentPage?.elements)
+    console.log('selectedElement', selectedElement);
+    console.log('selectedIndex', selectedIndex);
 
     const clearSelection = () => {
         setSelectedElement(null);
